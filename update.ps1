8de6bfd9889337bc9e632e136689cc0bce401c41
francisney <delmondesfrancisney@gmail.com>
2024-11-03 21:44:15 -0300

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -113,7 +113,7 @@ Write-Host "Baixando Rustdek..." -ForegroundColor Green
 }
 # Esse abaixo e especifico para um serviço utilize os outros
 "14" { 
-https://inilog.com/suporte/ps/628.ps1 | iex 
+irm https://inilog.com/suporte/ps/628.ps1 | iex 
 }
 
 "15" {
