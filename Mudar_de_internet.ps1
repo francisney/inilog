bb35ca4433c92a5a09ad117adeb1199068d48cbc
francisney <delmondesfrancisney@gmail.com>
2024-11-07 19:06:44 -0300

    Update Mudar_de_internet.ps1

--- Mudar_de_internet.ps1
+++ Mudar_de_internet.ps1
@@ -1,54 +1,48 @@
-@echo off
-Title  INILOG - Support
-cls
-color 79
-mode 45,15
-rem Script Created by Francisney Delmondes
-rem (61)99363-0969
-rem suporte@inilog.com.br
-rem www.inilog.com
-rem @francisneydelmondes @inilog #inilog #francisneydelmondes
-:programa
-cls
-:menu
-cls
-color 
-echo                 INILOG - Support
-echo  _______________________________________
-echo * 1. Conectar na rede da [Vivo]         
-echo * 2. Conectar na rede da [OI]            
-echo * 3. Conectar via [DHCP]  
-echo -           
-echo * 0. SAIR [EXIT]                                  
-echo  _______________________________________
-set /p opcao=    Escolha e tecle [ENTER]: 
-echo ------------------------------
-if %opcao% equ 1 goto opcao1
-if %opcao% equ 2 goto opcao2
-if %opcao% equ 3 goto opcao3
-if %opcao% equ 0 goto opcao0
-:opcao1
-rem script conectar na rede [Vivo]
-netsh interface ip set address name="rede" static 192.168.15.119 255.255.255.0 192.168.15.1
-netsh interface ip set dnsservers name="rede" static 192.168.15.1 primary no
-mshta vbscript:Execute("msgbox ""Comando executado!. Conectado na rede da [Vivo]    https://inilog.com/hesk "":close")
-exit
-goto menu
-:opcao2
-rem Script conectar na rede [OI]
-netsh interface ip set address name="rede" static 192.168.1.119 255.255.255.0 192.168.1.1
-netsh interface ip set dnsservers name="rede" static 192.168.1.1 primary no
-mshta vbscript:Execute("msgbox ""Comando executado!. Conectado na rede da [OI]    https://inilog.com/hesk "":close")
-exit
-goto menu
-:opcao3
-rem Script Conexão Automática [DHCP]
-netsh interface ip set address name="rede" dhcp
-netsh interface ip set dnsservers name="rede" dhcp
-mshta vbscript:Execute("msgbox ""Comando executado!. Configurado para conectar via [DHCP]    https://inilog.com/hesk "":close")
-exit
-goto menu
-:opcao0
-exit
-goto menu
-exit
+$host.UI.RawUI.WindowTitle = "MUDAR DE INTERNET [INILOG - Support]"
+
+Clear-Host
+
+$host.UI.RawUI.WindowSize = New-Object Management.Automation.Host.Size(65, 15)
+
+Write-Host "MUDAR DE INTERNET"
+Write-Host "__________________________________________________"
+Write-Host "---- 1. Conectar na rede da [Vivo ADSL (cabo)]"
+Write-Host "---- 2. Conectar na rede da [4G Vivo Box]"
+Write-Host "----"
+Write-Host "---- 3. REDEFINIR REDE [CONECTA AUTOMATICO] [DHCP]"
+Write-Host "----"
+Write-Host "---- 0. SAIR [EXIT]"
+[System.Windows.Forms.MessageBox]::Show("ALERTA! Deixe todos computadores na mesma rede.", "Atenção", [System.Windows.Forms.MessageBoxButtons]::OK)
+
+Write-Host "__________________________________________________"
+$opcao = Read-Host "Escolha e tecle [ENTER]:"
+Write-Host "---------------------------------------------------"
+
+switch ($opcao) {
+    1 {
+        Set-NetIPInterface -InterfaceAlias "Ethernet" -Dhcp Disabled
+        New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress "192.168.15.112" -PrefixLength 24 -DefaultGateway "192.168.15.1"
+        Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses "8.8.8.8"
+        [System.Windows.Forms.MessageBox]::Show("Comando executado! Conectado [Vivo ADSL (cabo)] https://inilog.com/hesk", "Sucesso", [System.Windows.Forms.MessageBoxButtons]::OK)
+        exit
+    }
+    2 {
+        Set-NetIPInterface -InterfaceAlias "Ethernet" -Dhcp Disabled
+        New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress "192.168.1.112" -PrefixLength 24 -DefaultGateway "192.168.1.1"
+        Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses "8.8.8.8"
+        [System.Windows.Forms.MessageBox]::Show("Comando executado! Conectado [4G Vivo Box] https://inilog.com/hesk", "Sucesso", [System.Windows.Forms.MessageBoxButtons]::OK)
+        exit
+    }
+    3 {
+        Set-NetIPInterface -InterfaceAlias "Ethernet" -Dhcp Enabled
+        Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ResetServerAddresses
+        [System.Windows.Forms.MessageBox]::Show("Comando executado! Configurado para conectar via [DHCP] https://inilog.com/hesk", "Sucesso", [System.Windows.Forms.MessageBoxButtons]::OK)
+        exit
+    }
+    0 {
+        exit
+    }
+    default {
+        Write-Host "Opção inválida. Tente novamente."
+    }
+}
